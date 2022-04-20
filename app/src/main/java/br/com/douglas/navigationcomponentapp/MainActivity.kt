d47944152777eb7137347c40fa1ca52d4e392f64
dougborbabass <douglas.borba@acad.pucrs.br>
2022-04-20 16:47:03 -0300

    ajustes de strings, cores e themas

--- app/src/main/java/br/com/douglas/navigationcomponentapp/MainActivity.kt
+++ app/src/main/java/br/com/douglas/navigationcomponentapp/MainActivity.kt
@@ -2,14 +2,30 @@ package br.com.douglas.navigationcomponentapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import br.com.douglas.navigationcomponentapp.ui.profile.ProfileFragment
-import br.com.douglas.navigationcomponentapp.ui.start.StartFragment
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var appBarConfig: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setSupportActionBar(myToolbar)
+        navController = findNavController(R.id.navHostFragment)
+        appBarConfig = AppBarConfiguration(navController.graph)
+
+        setupActionBarWithNavController(navController, appBarConfig)
     }
 
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfig) || super.onSupportNavigateUp()
+    }
 }
